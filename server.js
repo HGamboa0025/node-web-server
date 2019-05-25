00de0485caa6f683291fdb2a7cbfdc7d3aa77c75
Harold <harold.kybrd@gmail.com>
2019-05-25 21:10:56 +0800

    Added projects feature

--- server.js
+++ server.js
@@ -51,6 +51,13 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs',{
+        Title: 'Projects',
+        pageTitle: 'Projects Page'
+    });
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Unable to handle request'
